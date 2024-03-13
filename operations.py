9359b038ea7ca07b6b66e7aa0611ea18c61907a9
phandavinci <abisheksudhar@gmail.com>
2024-03-13 23:40:39 +0530

    structured and cleaned data

--- operations.py
+++ operations.py
@@ -4,10 +4,30 @@ import os
 
 transactions = os.path.join('env','transactions.csv')
 
-df= pd.DataFrame(pd.read_csv(transactions))
-print(df.columns)
+df = pd.read_csv(transactions)
 
-debitRecords = df[df['Debit Amount       ']==0.00] 
-creditRecords = df[df['Debit Amount       ']==0.00]
+df.columns = df.columns.str.strip()
+df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+df.to_csv(transactions, index=False)
+
+def split_narration(narration):
+    parts = narration.split('-')
+    if len(parts) > 6:
+        parts[2]+='-'+parts.pop(3)
+    return parts
+
+
+df[['Type', 'Name', 'ID', 'BankID', 'RefNo.', 'Comments']] = df['Narration'].apply(lambda x: pd.Series(split_narration(x)))
+
+
+
+df = df.drop(columns=['Narration', 'RefNo.'])
+
+df.to_csv(transactions, index=False)
+
+print("CSV file updated successfully.")
+
+debitRecords = df[df['Debit Amount']==0.00] 
+creditRecords = df[df['Debit Amount']==0.00]
 
 print(creditRecords, debitRecords)

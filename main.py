d09dadcff8b04fdc6b95845512626c18150cfc2c
phandavinci <abisheksudhar@gmail.com>
2024-03-27 20:16:00 +0530

    for merging all the files

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Running the scraping.py...........")
+import scraping
+print("Running the cleaning.py...........")
+import cleaning
+print("Running the converting.py..........")
+import converting

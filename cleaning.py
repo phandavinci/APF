1efbc56a14510a6e0b8288d0d88f2fb4784dfa1e
phandavinci <abisheksudhar@gmail.com>
2024-03-29 12:16:30 +0530

    updated cleaning

--- cleaning.py
+++ cleaning.py
@@ -4,6 +4,8 @@ import os
 
 transactions = os.path.join('env','transactions.csv')
 
+cleanedTransactions = os.path.join('env', 'cleanedTransactions.csv')
+
 df = pd.read_csv(transactions)
 
 df.columns = df.columns.str.strip()
@@ -18,26 +20,25 @@ def split_narration(narration):
 #splitting the comments
 def split_comment(df, comment):
     #xxyy&comment
-    #xx - priority
+    #xx - priority - n, w, d
     #yy - types - s, l, b, o, c, m, i
-    ## FL(last km)&(petrollitres) - fuel
-    dic = {'s':"Split", 'l': 'Lend', 'b': 'Borrow', 'o': 'Others', 'c': 'Credited', 'm': 'Myself', 'i':'Installment', 's':'Savings'}
+    ## f(last km)lbl(petrollitres) - fuel
+    dic = {'s':"Split", 'l': 'Lend', 'b': 'Borrow', 'o': 'Others', 'c': 'Credited', 'm': 'Myself', 'i':'Installment', 's':'Savings', 'n':'Need', 'w':'Want', 'd':'Desire'}
     s = [i for i in comment.strip().lower().split('lbl') if i]
     if len(s)==0: return [None]*3
-    if len(s)!=2: return ([None]*2)+[comment]
-    print(s)
+    if len(s)!=2 or (s[0][0] != 'n' and 'w' and 'd' and 'f') : return ([None]*2)+[comment]
     try:
-        parts = [s[0][:2], s[0][2:],s[1]]
-        if parts[0]=='fl':
-            parts[1] = 'LK: '+parts[1]
-            parts[2] = parts[2]+'ltr(s)'
+        parts = [s[0][0], s[0][1],s[1]]
+        if parts[0]=='f':
+            parts[1] = parts[1]+'ltr(s)'
+            parts[2] = 'LK: '+parts[2]
             return parts
         if parts[1][0]=='s':
             div = int(parts[1][1])
             df['Debit Amount'] = int(df['Debit Amount'])/div
             parts[1] = dic[parts[1]]+' of '+str(div)
             return parts
-        parts[1] = dic[parts[1]]
+        parts = [dic[parts[0]], dic[parts[1]], parts[2]]
         parts = [i.capitalize() for i in parts]
     except:
         return ([None]*2)+[comment]
@@ -52,7 +53,7 @@ df[['Priority', 'TypeOfPayment', 'Comment']] = df['Comments'].apply(lambda x: pd
 
 df = df.drop(columns=['Narration', 'RefNo.', 'Value Dat', 'Comments'])
 
-df.to_csv(transactions, index=False)
+df.to_csv(cleanedTransactions, index=False)
 
 print("CSV file updated successfully.")
 
